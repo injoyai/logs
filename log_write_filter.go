bdf43d3f07aed88a30536333d5971ebb43166834
injoy <1113655791@qq.com>
2023-12-27 14:14:24 +0800

    去除Hook,用Filter代替,实现消息总线Trunk,增加FormatterHandler

--- log_write_filter.go
+++ log_write_filter.go
@@ -21,14 +21,14 @@ func (this *Filter) Enable(b ...bool) {
 	this.enable = len(b) == 0 || b[0]
 }
 
-// SetRegexp 设置正则表达式
-func (this *Filter) SetRegexp(reg string) {
+// SetRegular 设置正则表达式
+func (this *Filter) SetRegular(reg string) {
 	this.reg, _ = regexp.Compile(reg)
 }
 
 // SetLike 设置模糊搜索
 func (this *Filter) SetLike(like string) {
-	this.SetRegexp(`.*` + like + `.*`)
+	this.SetRegular(`.*` + like + `.*`)
 }
 
 func (this *Filter) Valid(p []byte) bool {
