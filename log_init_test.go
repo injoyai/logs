672f29c5956ba3375c58d4960103ef7b84498b72
injoyai <1113655791@qq.com>
2025-07-22 11:56:00 +0800

    增加单元测试

---
+++ log_init_test.go
@@ -0,0 +1,15 @@
+package logs
+
+import (
+	"testing"
+	"time"
+)
+
+func TestSetWriter(t *testing.T) {
+	f := NewFile("./output/logs/debug.log")
+	DefaultDebug.SetWriter(f)
+	for {
+		Debug(make([]byte, 1024))
+		<-time.After(time.Millisecond * 10)
+	}
+}
