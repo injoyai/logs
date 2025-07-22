9aa3e8beed62caf2e2aa6e2a4aaf348f93740bc0
injoyai <1113655791@qq.com>
2025-07-22 11:57:31 +0800

    优化File,现在能根据文件大小自动分片,并去除了自动删除

--- log_write_file.go
+++ log_write_file.go
@@ -3,111 +3,123 @@ package logs
 import (
 	"errors"
 	"io"
-	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
-	"strings"
+	"strconv"
 	"time"
 )
 
 //==============================WriteFile==============================
 
+// NewFile 写入文件
+func NewFile(filename string, maxSize ...int64) io.Writer {
+	f := &File{
+		Filename: filename,
+	}
+	if len(maxSize) > 0 && maxSize[0] > 0 {
+		f.MaxSize = maxSize[0]
+	}
+	return f
+}
+
 // File 写入文件,自动打开关闭文件
 type File struct {
-	Name   string //实例名称
-	Dir    string //路径
-	Layout string //文件格式
+	Filename string //日志名称
+	MaxSize  int64  //最大文件大小
 
-	filename string         //现在打开的文件名称
-	file     io.WriteCloser //文件流
+	filename  string         //现在打开的文件名称
+	file      io.WriteCloser //文件流
+	filesize  int64          //当前文件大小
+	fileIndex int            //文件分片序号
+}
+
+func (this *File) addIndex(filename string) string {
+	if this.fileIndex == 0 {
+		return filename
+	}
+	name := filepath.Base(filename)
+	ext := filepath.Ext(filename)
+	return filepath.Join(
+		filepath.Dir(filename),
+		name[:len(name)-len(ext)]+"-"+strconv.Itoa(this.fileIndex)+filepath.Ext(filename),
+	)
+}
+
+func (this *File) open(filename string) error {
+	//新建文件(如果不存在),添加至文件最后
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModeAppend|os.ModePerm)
+	if err != nil {
+		return err
+	}
+	info, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	//赋值文件名和文件流,方便下次操作
+	this.filename = filename
+	this.file = file
+	this.filesize = info.Size()
+	return nil
+}
+
+func (this *File) close() error {
+	if this.file != nil {
+		return this.file.Close()
+	}
+	return nil
 }
 
 func (this *File) Write(p []byte) (int, error) {
 
-	//生成文件名
-	layout := strings.ReplaceAll(this.Layout, "{name}", this.Name)
-	filename := this.Dir + time.Now().Format(layout)
+	//生成文件名, todo 这里是否耗时严重?
+	originFilename := time.Now().Format(this.Filename)
+	filename := this.addIndex(originFilename)
 
 	//判断设置的文件地址是否有效
 	if len(filename) == 0 {
 		return 0, errors.New("无效文件地址")
 	}
 
-	//是否是新的文件,或文件流是否存在
-	if filename != this.filename || this.file == nil {
-		//获取文件不匹配
+	//判断文件夹是否存在,不存在则新建
+	os.MkdirAll(filepath.Dir(filename), 0666)
+
+	if this.file == nil || filename != this.filename ||
+		this.MaxSize > 0 && this.filesize+int64(len(p)) > this.MaxSize {
+
+		//文件名称发生变化,例如时间变化,或单个文件大小超出,新建文件
 		//则关闭之前的文件,并重新生成文件
-		if this.file != nil {
-			if err := this.file.Close(); err != nil {
+		this.close()
+
+		//重置文件序号
+		if this.file == nil || filename != this.filename {
+			this.fileIndex = 0
+		}
+
+		//生成带后缀的文件名称
+		for ; ; this.fileIndex++ {
+			filename = this.addIndex(originFilename)
+			//获取文件信息
+			_, err := os.Stat(filename)
+			if err != nil && !os.IsNotExist(err) {
 				return 0, err
+			} else if os.IsNotExist(err) {
+				break
 			}
 		}
 
-		//判断文件是否存在,不存在则新建
-		os.MkdirAll(filepath.Dir(filename), 0666)
-
-		//新建文件(如果不存在),添加至文件最后
-		file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModeAppend|os.ModePerm)
-		if err != nil {
-			log.Println(err)
+		if err := this.open(filename); err != nil {
 			return 0, err
 		}
-
-		//赋值文件名和文件流,方便下次操作
-		this.filename = filename
-		this.file = file
-
 	}
 
 	//写入数据
-	return this.file.Write(p)
-}
-
-// NewFile 写入文件
-func NewFile(name, dir, layout string) io.Writer {
-	f := &File{Name: name, Dir: dir, Layout: layout}
-	return f
-}
-
-var DefaultRemoveFile = NewRemoveFile(0)
-
-type RemoveFile struct {
-	SaveTime   time.Duration       //保存时间
-	RemoveFunc func(r *RemoveFile) //删除文件函数
-}
-
-func (this *RemoveFile) SetSaveTime(saveTime time.Duration) *RemoveFile {
-	this.SaveTime = saveTime
-	return this
-}
-
-func (this *RemoveFile) SetFunc(f func(r *RemoveFile)) *RemoveFile {
-	this.RemoveFunc = f
-	return this
-}
-
-func NewRemoveFile(saveTime time.Duration) *RemoveFile {
-	r := &RemoveFile{
-		SaveTime: saveTime,
-		RemoveFunc: func(r *RemoveFile) {
-			date := time.Now().Add(-r.SaveTime).Format("2006-01-02")
-			files, _ := ioutil.ReadDir(DefaultDir)
-			for _, v := range files {
-				if len(v.Name()) == 10 && v.Name() <= date {
-					_ = os.RemoveAll(DefaultDir + "/" + v.Name())
-				}
-			}
-		},
+	n, err := this.file.Write(p)
+	if err != nil {
+		return 0, err
 	}
-	go func(r *RemoveFile) {
-		for {
-			time.Sleep(time.Second * 5)
-			if r.SaveTime > 0 && r.RemoveFunc != nil {
-				r.RemoveFunc(r)
-			}
-			time.Sleep(time.Hour)
-		}
-	}(r)
-	return r
+
+	//记录文件大小
+	this.filesize += int64(n)
+	return n, nil
 }
